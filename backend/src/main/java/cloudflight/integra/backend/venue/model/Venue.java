ee1e4cc2a2a3a1917275ff8fe574ac0a50a88477
CalinNebunu <75206353+CalinNebunu@users.noreply.github.com>
2026-07-15 14:17:36 +0300

    feat(venue): implement venue model and dtos

---
+++ backend/src/main/java/cloudflight/integra/backend/venue/model/Venue.java
@@ -0,0 +1,73 @@
+package cloudflight.integra.backend.venue.model;
+
+import java.time.LocalDateTime;
+
+public class Venue {
+    private Long id;
+    // TODO: Replace with @ManyToOne when JPA is introduced
+    private Long managedBy;
+    private String name;
+    private String description;
+    private String address;
+    private LocalDateTime createdAt;
+
+    public Venue() {
+    }
+
+    public Venue(Long id, Long managedBy, String name, String description, String address, LocalDateTime createdAt) {
+        this.id = id;
+        this.managedBy = managedBy;
+        this.name = name;
+        this.description = description;
+        this.address = address;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getManagedBy() {
+        return managedBy;
+    }
+
+    public void setManagedBy(Long managedBy) {
+        this.managedBy = managedBy;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
