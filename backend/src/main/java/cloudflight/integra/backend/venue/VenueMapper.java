f6c3eeb14e09f9083e7202080bf35d9aa863203e
CalinNebunu <75206353+CalinNebunu@users.noreply.github.com>
2026-07-16 15:35:49 +0300

    fix(venue): fix warning for mapper

--- backend/src/main/java/cloudflight/integra/backend/venue/VenueMapper.java
+++ backend/src/main/java/cloudflight/integra/backend/venue/VenueMapper.java
@@ -4,9 +4,14 @@ import cloudflight.integra.backend.venue.model.Venue;
 import cloudflight.integra.backend.venue.model.VenueRequestDto;
 import cloudflight.integra.backend.venue.model.VenueResponseDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface VenueMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
     Venue toEntity(VenueRequestDto venueRequestDto);
+
     VenueResponseDto toResponseDto(Venue venue);
 }
