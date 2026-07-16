0de6848acea03ab1fc268fa7f380bbe316e18be0
CalinNebunu <75206353+CalinNebunu@users.noreply.github.com>
2026-07-16 14:55:06 +0300

    fix(exception): deleted redundant import

--- backend/src/main/java/cloudflight/integra/backend/exceptions/ApiErrorResponse.java
+++ backend/src/main/java/cloudflight/integra/backend/exceptions/ApiErrorResponse.java
@@ -1,7 +1,5 @@
 package cloudflight.integra.backend.exceptions;
 
-import org.springframework.http.HttpStatus;
-
 import java.time.LocalDateTime;
 
 public record ApiErrorResponse(LocalDateTime timestamp, int status, String error, String message, String path) {
