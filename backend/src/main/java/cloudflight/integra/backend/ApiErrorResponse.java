33f9b23d81307946e9204a8af28a873e6d80dfb2
Avasilcai Andrei-Cristian <andreivasile2014@gmail.com>
2026-07-16 18:23:14 +0300

    ApiErrorResponse class added

---
+++ backend/src/main/java/cloudflight/integra/backend/ApiErrorResponse.java
@@ -0,0 +1,6 @@
+package cloudflight.integra.backend;
+
+import java.time.LocalDateTime;
+
+public record ApiErrorResponse(LocalDateTime timestamp, int status, String error, String message, String path) {
+}
