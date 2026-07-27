36df6b7ac34e71fdd3422d1f365ee1655c05a246
Oana N. <oana.nourescu@gmail.com>
2026-07-27 19:05:39 +0300

    fix: ensure OpenAPI docs & code generation relies on in-memory db + other minor improvements
    - added app properties for openapi spring profile
    - made sure tests & openAPI custom boot run use h2 db
    - modified the postgres readme
    - added run config for backend in the project
    - made sure tests run with the test spring profile
    - use spotless check instead of editorconfig check to ensure consistency

--- backend/src/test/java/cloudflight/integra/backend/BackendApplicationTests.java
+++ backend/src/test/java/cloudflight/integra/backend/BackendApplicationTests.java
@@ -2,7 +2,9 @@ package cloudflight.integra.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class BackendApplicationTests {
 
