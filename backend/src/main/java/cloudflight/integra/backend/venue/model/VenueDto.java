612b9828540a27580228c4c03397627cff98fc66
Avasilcai Andrei-Cristian <andreivasile2014@gmail.com>
2026-08-10 19:33:42 +0300

    fixed spotless check fail

--- backend/src/main/java/cloudflight/integra/backend/venue/model/VenueDto.java
+++ backend/src/main/java/cloudflight/integra/backend/venue/model/VenueDto.java
@@ -1,14 +1,13 @@
 package cloudflight.integra.backend.venue.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
 
 public record VenueDto(
-        @Schema(accessMode = Schema.AccessMode.READ_ONLY)
-        @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+        @Schema(accessMode = Schema.AccessMode.READ_ONLY) @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         Long id,
 
         Long managedBy,
@@ -16,6 +15,5 @@ public record VenueDto(
         String description,
         @NotBlank String address,
 
-        @Schema(accessMode = Schema.AccessMode.READ_ONLY)
-        @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+        @Schema(accessMode = Schema.AccessMode.READ_ONLY) @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         LocalDateTime createdAt) {}
