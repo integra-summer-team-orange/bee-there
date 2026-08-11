7effc98540b5a3a83a042c6690c26afd6cc2fd89
CalinBedea <75206353+CalinBedea@users.noreply.github.com>
2026-08-11 17:11:07 +0300

    fix(inventory): fix error message not showing when trying to create inventory with invalid quantities (#54)

--- backend/src/main/java/cloudflight/integra/backend/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/cloudflight/integra/backend/exceptions/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -61,8 +62,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
             MethodArgumentNotValidException ex, HttpServletRequest request) {
-        List<String> errorMessages = ex.getBindingResult().getFieldErrors().stream()
-                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+
+        List<String> errorMessages = ex.getBindingResult().getAllErrors().stream()
+                .map(error -> {
+                    if (error instanceof FieldError fieldError) {
+                        return fieldError.getField() + ": " + fieldError.getDefaultMessage();
+                    } else {
+                        return error.getDefaultMessage();
+                    }
+                })
                 .toList();
 
         ErrorResponse response = new ErrorResponse(
