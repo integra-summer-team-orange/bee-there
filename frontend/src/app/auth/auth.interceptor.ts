c2c356bf3484b8c52cdd372316851e63fd3e018f
Darius Szocs <dariusszocs11@gmail.com>
2026-09-08 19:32:28 +0300

    fix(user) - fixed card layout when text exceeds space

--- frontend/src/app/auth/auth.interceptor.ts
+++ frontend/src/app/auth/auth.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 //TODO: delete this when login and register is implemented
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJpb25lbDFAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE3ODg3NzQ5MTMsImV4cCI6MTc4ODg2MTMxM30.YagaOGnPuRLf_qsqgrTxAxkpg8XQy-rgf4MHQ7I57G8';
+  const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJpb25lbDFAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE3ODg4ODQwNjQsImV4cCI6MTc4ODk3MDQ2NH0.WGkCYlQPfGvwXTEgl4p9F8nQf6GeryxFN0KIMn0XbLg';
 
 
   const authReq = req.clone({
