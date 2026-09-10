6ac4a3e29d795f73ee7158598ae2c22cbc9da808
Darius Szocs <dariusszocs11@gmail.com>
2026-09-10 17:39:37 +0300

    fix(user) - rebased and updated to work with new app version

--- frontend/src/app/auth/auth.interceptor.ts
+++ frontend/src/app/auth/auth.interceptor.ts
@@ -1,14 +1,19 @@
+import { inject } from '@angular/core';
 import { HttpInterceptorFn } from '@angular/common/http';
+import { SessionService } from '../core/services/session.service';
 
-//TODO: delete this when login and register is implemented
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJpb25lbDFAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE3ODg4ODQwNjQsImV4cCI6MTc4ODk3MDQ2NH0.WGkCYlQPfGvwXTEgl4p9F8nQf6GeryxFN0KIMn0XbLg';
+  const session = inject(SessionService);
+  const token = session.getToken();
 
+  if (!token) {
+    return next(req);
+  }
 
   const authReq = req.clone({
     setHeaders: {
-      Authorization: `Bearer ${token}`
-    }
+      Authorization: `Bearer ${token}`,
+    },
   });
 
   return next(authReq);
